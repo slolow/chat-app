e07cb9777bbb6e91fcf9b8bd636ad752b564f0fc
slolow <laszlostarost@googlemail.com>
2020-05-28 01:40:06 +0200

    removed comment out statements

--- static/index.js
+++ static/index.js
@@ -3,9 +3,7 @@ function alertName () {
   document.querySelector('#form-name').scrollIntoView();
 }
 
-function fillInName () {
-  /*document.querySelector('#name').remove();
-  document.querySelector('#submit-name').remove();*/
+function fillInNameForm () {
   const input = createInput(id='name', autocomplete='off', autofocus=true, placeholder='name');
   const submit = createSubmit(id='submit-name', disbaled=true);
   document.querySelector('#form-name').appendChild(input);
@@ -76,60 +74,24 @@ function initializeLocalStorage(name, value) {
       localStorage.setItem(name, value);
 }
 
-initializeLocalStorage(name='counter', value=1);
-
 function linkChatMenuToChatCarousel() {
   document.querySelectorAll('.chatLinkItem').forEach(link => {
     link.onclick = function () {
-      $('.carousel').carousel(parseInt(link.id.substr(-1)));
+      carouselNumber = parseInt(link.id.substr(-1));
+      $('.carousel').carousel(carouselNumber);
     }
   });
 }
-//initializeLocalStorage(name='chatCounter', value=0);
-/*if (!localStorage.getItem('counter'))
-    localStorage.setItem('counter', 1);*/
 
-//load existing chats
-/*function loadChatsFromLocalStorage () {
-  for (i = 0; i < localStorage.length; i++) {
-    if (keys(localStorage)[i].startsWith('chat-name-')) {
-      console.log(keys(localStorage)[i]);
-    }
-  }
-  if (localStorage.getItem('chat-name-0')) {
-
-  }
-  const chats = str.startswith()
-}*/
-
-
+initializeLocalStorage(name='counter', value=1);
 
 document.addEventListener('DOMContentLoaded', function() {
 
   // Show form for user name if no name in local storage
   if (!localStorage.getItem('name')) {
     alertName();
-    fillInName();
-    // create input tag and submit button in form
-    /*const input = createInput(id='name', autocomplete='off', autofocus=true, placeholder='name');
-    const submit = createSubmit(id='submit-name', disbaled=true);
-    document.querySelector('#form-name').appendChild(input);
-    document.querySelector('#form-name').appendChild(submit);
-    // disable submit button if no text in input
-    disableButton(inputId='#name', submitId='#submit-name');
-    // Add user name to local storage and remove form-name
-    document.querySelector('#form-name').onsubmit = function() {
-      const name = document.querySelector('#name').value;
-      localStorage.setItem('name', name);
-      document.querySelector('#form-name').remove();
-      alert('Gracias Amigo! Meet interesting people in our chatroom or create a new chatroom with a topic of your choice');
-      return false;
-    }*/
+    fillInNameForm();
   }
-
-  //load existing chats
-
-  /*if (!localStorage.getItem('cha'))*/
 
   //create a new chat
   document.querySelector('#create-new-chat').onclick = function() {
@@ -139,7 +101,6 @@ document.addEventListener('DOMContentLoaded', function() {
   }
     else {
       alert('Fill out the form to create a new chat');
-      // here load form (name of chat, topic and background-color, color)
       const form = createForm(id='form-create-new-chat');
       const inputChat = createInput(id='chat-name', autocomplete='off', autofocus=true, placeholder='chat name');
       const inputTopic = createInput(id='topic', autocomplete='off', autofocus=true, placeholder='topic');
@@ -148,38 +109,20 @@ document.addEventListener('DOMContentLoaded', function() {
       form.appendChild(inputChat);
       form.appendChild(inputTopic);
       form.appendChild(submit);
-      /*document.querySelector('#form-create-new-chat').appendChild(inputChat);
-      document.querySelector('#form-create-new-chat').appendChild(inputTopic);
-      document.querySelector('#form-create-new-chat').appendChild(submit);*/
       disableButton(inputId='#chat-name', submitId='#submit-chat-name');
       inputChat.scrollIntoView();
-      //create new chat
-      /*document.querySelector('#form-create-new-chat').onsubmit = function() {*/
       form.onsubmit = function() {
-        /*const chatName = document.querySelector('#chat-name').value;*/
         const chatName = inputChat.value;
-        //let chatCounter = localStorage.getItem('chatCounter');
-        //localStorage.setItem('chat-name-' + chatCounter, chatName);
         form.remove();
-        //return false;
         let counter = localStorage.getItem('counter');
         const divCarouselItem = createDiv(id="carousel-item-" + counter, classList="carousel-item");
-        /*const divCarouselItem = document.createElement('div');
-        // change number item-2 with counter!
-        divCarouselItem.id = "carousel-item-" + counter;
-        divCarouselItem.classList.add("carousel-item");*/
         document.querySelector('.carousel-inner').appendChild(divCarouselItem);
         const h1 = document.createElement('h1');
-        // change 'second chat with name of the chat'
         h1.innerHTML = chatName;
         document.querySelector('#carousel-item-' + counter).appendChild(h1);
 
         // add chat link in menu bar
         const span = createSpan(id="link-" + counter, classList='chatLinkItem', innerHTML=chatName)
-        /*const span = document.createElement('span');
-        span.classList.add('chatLinkItem');
-        span.id = "link-" + counter;
-        span.innerHTML = chatName;*/
         document.querySelector('#chatLinkContainer').appendChild(span);
 
         localStorage.setItem('chat-name-' + counter, chatName);
@@ -187,29 +130,9 @@ document.addEventListener('DOMContentLoaded', function() {
         localStorage.setItem('counter', counter);
 
         document.querySelector('#create-new-chat').disabled = false;
-        //show selected chat in menu
-        /*document.querySelectorAll('.chatLinkItem').forEach(link => {
-          link.onclick = function () {
-            $('.carousel').carousel(parseInt(link.id.substr(-1)));
-          }
-        });*/
+
         linkChatMenuToChatCarousel();
     }
   }
   };
-
-  //show selected chat in menu
-  /*const chatLinkItemClass = document.querySelector('.chatLinkItem')
-  chatLinkItemClass.onclick = function () {
-
-    $('.carousel').carousel(2)
-  }*/
-
-/*  document.querySelectorAll('.chatLinkItem').forEach(link => {
-    link.onclick = function () {
-      alert('Clicked on: ' + link.id.substr(-1));
-      $('.carousel').carousel(parseInt(link.id.substr(-1)));
-    }
-  });*/
-
 });
