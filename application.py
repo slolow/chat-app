e556bb78204a6081b21793cfcdafa559ad457616
Laszlo Starost <laszlo.starost@iee.fraunhofer.de>
2020-06-08 23:44:33 +0200

    test application

--- application.py
+++ application.py
@@ -9,47 +9,26 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
-messages = {}
-chatRooms = []
+title = {'name_page': 'name',
+         'new_chat_page': 'create  new chat'}
+
+h1 = {'name_page': 'Welcome Amigo! Please, tell me your name.',
+          'new_chat_page': 'Create a new chat my friend!'}
+
+buttonTexts = {'name_page': 'let\'s chat',
+               'new_chat_page': 'create'}
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-
-@socketio.on("new message")
-def message(data):
-    message = data["message"]
-    chatRoomName = data["chatRoomName"]
-    user = data["user"]
-    time = datetime.now()
-    time_str = time.strftime("%d %m %Y %H:%M:%S")
-
-    messages[chatRoomName].append({'time': time_str, 'message': message, 'user': user})
-    messageId = len(messages[chatRoomName])
-    # only store 100 newest messages per chat room
-    if messageId > 100:
-        messages[chatRoomName].remove(messages[chatRoomName][0])
-
-    emit("latest messages", {"message": message, "chatRoomName": chatRoomName, "messageId": messageId, "time": time_str, "user": user}, broadcast=True)
+# find out how to combinate /name and /new-chat to one route
+@app.route("/name")
+def prompt_name():
+    return render_template("form.html", title=title['name_page'], h1=h1['name_page'], buttonText=buttonTexts['name_page'])
 
 
-@socketio.on("chat room request")
-def getChatRoom(data):
-    chatRoomName = data["chatRoomName"]
-    chatRoomMessages = messages[chatRoomName]
-    emit("chat room access", {"chatRoomName": chatRoomName, "messages": chatRoomMessages}, broadcast=False)
-
-
-@socketio.on("create new chat room")
-def createChatRoom(data):
-    chatRoomName = data["chatRoomName"]
-    user = data["user"]
-    if chatRoomName not in chatRooms:
-        chatRooms.append(chatRoomName)
-        messages[chatRoomName] = []
-        nameIsAvaible = True
-    else:
-        nameIsAvaible = False
-    emit("new chat room created", {"chatRoomName": chatRoomName, "nameIsAvaible": nameIsAvaible, "user": user}, broadcast=True)
+@app.route("/new-chat")
+def prompt_new_chat():
+    return render_template("form.html", title=title['new_chat_page'], h1=h1['new_chat_page'], buttonText=buttonTexts['new_chat_page'])
