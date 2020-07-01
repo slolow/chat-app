c9b595f10b124d465b8d3aeebf80209d0d664185
slolow <laszlostarost@googlemail.com>
2020-07-01 13:34:56 +0200

    only show draw page if user loged in

--- static/draw.js
+++ static/draw.js
@@ -1,72 +1,100 @@
-document.addEventListener('DOMContentLoaded', () => {
+// Only show new chat prompt page if user name in local Storage
+if (!localStorage.getItem('username')) {
+  location.replace('/name');
+}
 
-    // state
-    let draw = false;
+else {
 
-    // elements
-    let points = [];
-    let lines = [];
-    let svg = null;
+  document.addEventListener('DOMContentLoaded', () => {
 
-    function render() {
+      var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
-        // create the selection area
-        svg = d3.select('#draw')
-                .attr('height', window.innerHeight)
-                .attr('width', window.innerWidth);
+      // state
+      let draw = false;
 
-        svg.on('mousedown', function() {
-            draw = true;
-            const coords = d3.mouse(this);
-            draw_point(coords[0], coords[1], false);
-        });
+      // elements
+      let points = [];
+      let lines = [];
+      let svg = null;
 
-        svg.on('mouseup', () =>{
-            draw = false;
-        });
+      function render() {
 
-        svg.on('mousemove', function() {
-            if (!draw)
-                return;
-            const coords = d3.mouse(this);
-            draw_point(coords[0], coords[1], true);
-        });
+          // create the selection area
+          svg = d3.select('#draw')
+                  .attr('height', window.innerHeight)
+                  .attr('width', window.innerWidth);
 
-        document.querySelector('#erase').onclick = () => {
-            for (let i = 0; i < points.length; i++)
-                points[i].remove();
-            for (let i = 0; i < lines.length; i++)
-                lines[i].remove();
-            points = [];
-            lines = [];
-        }
+          svg.on('mousedown', function() {
+              draw = true;
+              const coords = d3.mouse(this);
+              draw_point(coords[0], coords[1], false);
+          });
 
-    }
+          svg.on('mouseup', () =>{
+              draw = false;
+          });
 
-    function draw_point(x, y, connect) {
+          svg.on('mousemove', function() {
+              if (!draw)
+                  return;
+              const coords = d3.mouse(this);
+              draw_point(coords[0], coords[1], true);
+          });
 
-        const color = document.querySelector('#color-picker').value;
-        const thickness = document.querySelector('#thickness-picker').value;
+          document.querySelector('#erase').onclick = () => {
+              for (let i = 0; i < points.length; i++)
+                  points[i].remove();
+              for (let i = 0; i < lines.length; i++)
+                  lines[i].remove();
+              points = [];
+              lines = [];
+          }
 
-        if (connect) {
-            const last_point = points[points.length - 1];
-            const line = svg.append('line')
-                            .attr('x1', last_point.attr('cx'))
-                            .attr('y1', last_point.attr('cy'))
-                            .attr('x2', x)
-                            .attr('y2', y)
-                            .attr('stroke-width', thickness * 2)
-                            .style('stroke', color);
-            lines.push(line);
-        }
+          socket.on('connect', () => {
+            document.querySelector('#send').onclick = () => {
+              socket.emit('send drawing', {'points': points, 'lines': lines})
 
-        const point = svg.append('circle')
-                         .attr('cx', x)
-                         .attr('cy', y)
-                         .attr('r', thickness)
-                         .style('fill', color);
-        points.push(point);
-    }
+              // set time out before runing afterTimeOut function. Otherwise new chat is not emit to flask
+              window.setTimeout('afterTimeOut()', 1);
+              /*for (let i = 0; i < points.length; i++) {
+                //console.log(points[i].attr('cx'));
+                draw_point(points[i].attr('cx'), points[i].attr('cy'), false);
+              }*/
+            }
+          });
 
-    render();
-});
+      }
+
+      function draw_point(x, y, connect) {
+
+          const color = document.querySelector('#color-picker').value;
+          const thickness = document.querySelector('#thickness-picker').value;
+
+          if (connect) {
+              const last_point = points[points.length - 1];
+              const line = svg.append('line')
+                              .attr('x1', last_point.attr('cx'))
+                              .attr('y1', last_point.attr('cy'))
+                              .attr('x2', x)
+                              .attr('y2', y)
+                              .attr('stroke-width', thickness * 2)
+                              .style('stroke', color);
+              lines.push(line);
+          }
+
+          const point = svg.append('circle')
+                           .attr('cx', x)
+                           .attr('cy', y)
+                           .attr('r', thickness)
+                           .style('fill', color);
+          points.push(point);
+      }
+
+      render();
+  });  
+
+}
+
+function afterTimeOut() {
+  location.replace('/');
+}
